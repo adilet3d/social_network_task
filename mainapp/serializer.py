dfc183a89f9da606cdce13ef2566076349a16495
adilet <adiletganiev7@gmail.com>
2023-02-03 11:45:49 +0600

    serializers and views

--- mainapp/serializer.py
+++ mainapp/serializer.py
@@ -1,9 +1,37 @@
-from rest_framework import serializers,permissions
+from rest_framework import serializers,exceptions
 from mainapp.models import User,Post
 
 class UserSerializer(serializers.Serializer):
     class Meta:
         model=User
         fields=(
-            'id','name',''
+            'id','name','second_name','date_of_birth','country',
         )
+
+class PostSerializer(serializers.Serializer):
+    user= UserSerializer
+    class Meta:
+        model=Post
+        fields=(
+            'id','text','image','create_at',
+        )
+        read_only_field=(
+            'user',
+        )
+
+
+class RegistrationSerializer(serializers.Serializer):
+    username= serializers.CharField()
+    password= serializers.CharField()
+    email= serializers.CharField()
+    def validate_password(self,value):
+        if len(value) <8:
+            raise exceptions.ValidationError('Password is too short')
+        elif len (value) >24:
+            raise exceptions.ValidationError ('Password is too long')
+        return value
+
+
+class AuthorizationSerializer(serializers.Serializer):
+    username= serializers.CharField()
+    password= serializers.CharField()
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from mainapp.models import User,Post
+from mainapp.serializer import (
+    UserSerializer,PostSerializer,AuthorizationSerializer,\
+        RegistrationSerializer
+)
+from django.contrib.auth import get_user_model
+User=get_user_model()
+from django.contrib.auth.hashers import check_password
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import permissions
+from rest_framework.decorators import action
 
-# Create your views here.
+
+
+class UserView(ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+
+
+class PostView(ModelViewSet):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
+
+
+
+class RegistrationView(APIView):
+    def post(self,request):
+        serializer= RegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data=serializer.validated_data
+
+
+        username= data.get('username')
+        email=data.get('email')
+        password=data.get('password')
+        if User.objects.filter(username=username).exists():
+            return Response({'message':'User with such name is already exists'})
+        user= User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+
+        )
+        token=Token.objects.create(user=user)
+        return Response({'token':token.key})
+
+class AuthorizationView(APIView):
+    def post(self, requset):
+        serializer=AuthorizationSerializer
+        serializer.is_valid(raise_exception=True)
+        data=serializer.validated_data
+        
+        
+        username=data.get('username')
+        password=data.get('password')
+        user= User.objects.filter(username=username).first()
+        
+        if user is not None:
+            if check_password(password, user.password):
+                token,_=Token.objects.get_or_create(user=user)
+                return Response({'token:':token.key})
+            return Response ({'error':'Password is not valid'}, status=400)
+        return Response({'error':'This username is not registred'},status=400)
+
